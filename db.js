68312c639cae6053b781dbf722f38ac5f4206136
BUM <pc7537@naver.com>
2023-11-24 02:37:14 +0900

    Ver.2

--- db.js
+++ db.js
@@ -4,16 +4,20 @@ import nunjucks from 'nunjucks';
 import bodyParser from 'body-parser';
 
 const app = express();
-app.set('view engine', 'html'); //main.html -> main.(html)
+
 
 app.use(bodyParser.urlencoded({ extended: false })); // express 기본 모듈 사용
 app.use(bodyParser.json());
 
+
+
 //nunjucks setting
+app.set('view engine', 'html'); //main.html -> main.(html)
 //템플릿 엔진을 사용할 때 어떤 위치에서 파일을 찾을건지 경로 입력
-nunjucks.configure('views', {
+nunjucks.configure('views', { //views 폴더가 nunjucks파일의 위치가 됨.
+  autoescape:true,
   watch : true, //html파일이 수정될 경우, 다시 반영 후 렌더링
-  express : app //express자체가 어떤 객체를 나타내는지 앞서 선언한 app을 입력
+  express : app, //express자체가 어떤 객체를 나타내는지 앞서 선언한 app을 입력
 })
 
 
@@ -45,34 +49,26 @@ const Gas_emission_year = mongoose.model('Gas_emission_year', Gas_emission_yearS
 
 
 app.get('/', async (req, res) => {
-
   //print chart
   try {
     //find data
     let chartValue = await emission_factor.find({}); 
     //find the average
     let chartAvg = await emission_factor.aggregate([ { $group : { _id : null, averageValue: { $avg : '$tonCO2eq' }}} ]);
-    // chart sort
-    let chartSort = await emission_factor.find({}).sort({'tonCO2eq':1});
-    // reverse sort
-    let chartReverseSort = await emission_factor.find({}).sort({'tonCO2eq':-1});
 
     console.log(chartValue); // 결과를 확인하기 위해 로그로 출력
     console.log(chartAvg);
-    console.log(chartSort);
-    console.log(chartReverseSort);
+
     // JSON 데이터를 문자열로 변환하여 Nunjucks에 전달
-    const data = JSON.stringify(chartValue);
-    
-    res.render('index',{list: chartValue , data : data , chartAvg});
+    res.render('index',{list: chartValue , data : JSON.stringify(chartValue), chartAvg : JSON.stringify(chartAvg)});
   } catch (err) {
       console.error(err);
       res.status(500).send('Internal Server Error');
   }
 });
 
-app.get('/year', async (req, res) => {
 
+app.get('/year', async (req, res) => {
   //print chart
   try {
     let chartValue = await Gas_emission_year.find({});
@@ -86,6 +82,28 @@ app.get('/year', async (req, res) => {
       res.status(500).send('Internal Server Error');
   }
 });
+
+app.get('/sort', async (req, res) => {
+  // chart sort
+  let chartSort = await emission_factor.find({}).sort({'tonCO2eq':1});
+  //find the average
+  let chartAvg = await emission_factor.aggregate([ { $group : { _id : null, averageValue: { $avg : '$tonCO2eq' }}} ]);
+  console.log(chartSort);
+
+  res.render('sort', {chartSort : chartSort, chartSortValue:JSON.stringify(chartSort), chartAvg : JSON.stringify(chartAvg)})
+})
+
+
+app.get('/reverseSort', async (req, res) => {
+  // chart sort
+  let chartReverseSort = await emission_factor.find({}).sort({'tonCO2eq':-1});
+  console.log(chartReverseSort);
+  //find the average
+  let chartAvg = await emission_factor.aggregate([ { $group : { _id : null, averageValue: { $avg : '$tonCO2eq' }}} ]);
+  console.log(chartReverseSort);
+
+  res.render('reverseSort', {chartReverseSort : chartReverseSort,chartReverseSortValue:JSON.stringify(chartReverseSort), chartAvg : JSON.stringify(chartAvg)})
+})
 
 
 app.listen(3000, () => {
